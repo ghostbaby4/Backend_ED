5dfe805942bb0d015e0b3bf4a2525e032e9ba537
ghostbaby4 <vanegajudith26@gmail.com>
2026-05-24 19:46:03 -0600

    migracion contacto

--- Config/settings.py
+++ Config/settings.py
@@ -88,16 +88,19 @@ WSGI_APPLICATION = 'Config.wsgi.application'
 #}
 
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if isinstance(DATABASE_URL, bytes):
-    DATABASE_URL = DATABASE_URL.decode()
-
-DATABASE_URL = DATABASE_URL.strip()
-
-DATABASES = {
-    'default': dj_database_url.parse(DATABASE_URL)
-}
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
